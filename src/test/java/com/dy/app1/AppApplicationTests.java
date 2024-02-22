c7476de31da071a8e40672a079f26eafc235a078
danekim <tyxmfh@naver.com>
2024-02-22 22:13:44 +0900

    Initial commit

---
+++ src/test/java/com/dy/app1/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dy.app1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
